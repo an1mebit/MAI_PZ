bbc0ae510ef6c6f5c165e10eb231d98b8837b8fe
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-09-13 14:38:51 +0300

    Create class_mas.cpp

---
+++ PZ1/class_mas.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+class mas
+{
+private:
+
+	int size;
+	int* arr;
+
+public:
+
+	mas(int count);
+	mas(const  mas &array);
+	~mas();
+
+	void print();
+	void add(mas & arr);
+	void summ(mas & array);
+	void subt(mas & array);
+
+};
+
+mas::mas(int count) : size(count)
+{
+	arr = new int[size];
+	for (int i = 0; i < size; i++)
+		arr[i] = i;
+}
+
+mas::mas(const mas& array): size(array.size), arr(nullptr)
+{
+	arr = new int[size];
+	for (int i = 0; i < size; i++)
+		arr[i] = array.arr[i];
+}
+
+mas::~mas()
+{
+	//delete[] arr;
+}
+
+void mas::print()
+{
+	for (int i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+void mas::add(mas & arr)
+{
+	int val = arr.size;
+	arr.arr[arr.size] = val;
+	arr.size++;
+
+	if (arr.size > 100 || arr.size < -100)
+	{
+		std::cout << "TOO MUCH" << std::endl;
+		exit(-1);
+	}
+}
+
+void mas::summ(mas & array)
+{
+	int sum1 = 0; int sum2 = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum1 += arr[i];
+	}
+
+	for (int i = 0; i < array.size; i++)
+	{
+		sum2 += array.arr[i];
+	}
+
+	std::cout << "Summ of two dinamic massives: " << sum1 + sum2 << std::endl;
+}
+
+void mas::subt(mas& array)
+{
+	int sum1 = 0; int sum2 = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum1 += arr[i];
+	}
+
+	for (int i = 0; i < array.size; i++)
+	{
+		sum2 += array.arr[i];
+	}
+
+	std::cout << "Substraction of two dinamic massives: " << sum1 - sum2 << std::endl;
+}
+
+int main()
+{
+	mas arr(10);
+	arr.add(arr);
+	arr.print();
+
+
+	mas arr2 = arr;
+	arr2.print();
+
+	std::cout << std::endl;
+	arr.summ(arr2);
+	arr.subt(arr2);
+
+	return 0;
+}
