de1b4d3d90b1d1250fc3aad4308a2658bd7e6538
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-10-25 15:14:12 +0300

    Update doubleList.cpp

--- pz5/doubleList.cpp
+++ pz5/doubleList.cpp
@@ -5,9 +5,17 @@ using namespace std;
 template <class T>
 struct Elem
 {
-	Elem* next;
-	Elem* prev;
+	Elem<T>* next;
+	Elem<T>* prev;
 	T data;
+
+	int uid;
+	int calls = 0;
+
+	Elem(int id)
+	{
+		uid = id;
+	}
 };
 
 template <class T>
@@ -16,6 +24,8 @@ class List
 	Elem<T>* head;
 	Elem<T>* tail;
 	size_t size;
+
+
 public:
 	List() : head(nullptr), tail(nullptr), size(0) {}
 	~List() { clear(); }
@@ -23,6 +33,9 @@ public:
 	void pushBack(const T& data);
 	void clear();
 	void show();
+	T get(int id);
+	void swap(Elem<T>* l1, Elem<T>* l2);
+	void sort(Elem<T>* lst);
 };
 
 template<class T>
@@ -58,10 +71,62 @@ void List<T>::show()
 	while (head)
 	{
 		Elem<T>* current = head;
-		cout << "{ " << head->data << " } ";
+		cout << head->uid << "{ " << head->data << " } ";
 		head = head->next;
 	}
 	cout << endl;
+}
+
+template<class T>
+T List<T>::get(int id)
+{
+	Elem<T>* current = head;
+	while (head->uid != id && head->next != nullptr)
+		head = head->next;
+
+	if (head->uid == id)
+	{
+		head->calls++;+		head	0x00000000 <NULL>	Elem<int> *
+
+		sort(head);
+		return head->data;
+	}
+	else
+	{
+		cout << "Getter error:";
+		cout << "No matching records were found. Returned first element." << endl;
+		return head->data;
+	}
+}
+
+template<class T>
+void List<T>::swap(Elem<T>* l1, Elem<T>* l2)
+{
+	T pdata = l1->data;
+	int id = l1->uid;
+	int cls = l1->calls;
+
+	l1->data = l2->data;
+	l1->uid = l2->uid;
+	l1->calls = l2->calls;
+
+	l2->data = pdata;
+	l2->uid = id;
+	l2->calls = cls;
+
+}
+
+template<class T>
+void List<T>::sort(Elem<T>* lst)
+{
+	while (lst->prev != nullptr)
+	{
+		if (lst->prev->calls < lst->calls)
+			swap(lst->prev, lst);
+		else break;
+
+		lst = lst->prev;
+	}
 }
 
 int main()
@@ -70,5 +135,12 @@ int main()
 
 	for (int i = 0; i < 10; i++)
 		datalist->pushBack(i);
+	datalist->show();
+	cout << "===============================" << endl;
+	
+	datalist->get(4);
+	datalist->get(4);
+	datalist->get(2);
+	datalist->get(9);
 	datalist->show();
 }
