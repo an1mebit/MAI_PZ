4957dae2a18cfbb4ac2898c92c85c51293136b27
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-10-31 16:38:49 +0300

    Create grapth.cpp

---
+++ pz4/grapth.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+class Graph
+{
+    int numVertices;
+    list<int> *adjLists;
+
+public:
+    Graph(int V);
+    void addEdge(int src, int dest);
+};
+
+Graph::Graph(int vertices)
+{
+    numVertices = vertices;
+    adjLists = new list<int>[vertices];
+}
+
+void Graph::addEdge(int src, int dest)
+{
+    adjLists[src].push_front(dest);
+}
+
+
+int main()
+{
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+
+    return 0;
+}
