b1e109836d1c0c5d6ac079f1b5c8cf09ad60b863
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-10-31 15:55:43 +0300

    Create vector.cpp

---
+++ pz4/vector.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+
+using namespace std;
+int main()
+{
+	cout << "Enter a size of two vectors" << endl;
+	int num1, num2;
+	int sum1 = 0, sum2 = 0;
+	cin >> num1 >> num2;
+
+	vector<int> vec1(num1), vec2(num2);
+
+	cout << "First vector: ";
+	for (int& i : vec1)
+	{
+		i = rand() % 100 + 1;
+		sum1 += i;
+		cout << i << " ";
+	}
+	cout << endl;
+
+	map<int, int> number;
+	cout << "Second vector";
+	for (int& i : vec2)
+	{
+		i = rand() % 100 + 1;
+		sum2 += i;
+
+		if (find(vec1.begin(), vec1.end(), i) != vec1.end()) number[i]++;
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << "sum of numbers of first vector is: " << sum1 / num1 << endl;
+	cout << "sum of numbers of second vector is: " << sum2 / num2 << endl;
+
+	cout << "same elements: ";
+	for (pair<int, int> tmp : number)
+		cout << tmp.first << " ";
+	cout << endl;
+}
