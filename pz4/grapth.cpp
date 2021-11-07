7e0c2ab51600097cc45717908052d06c7ffe88d0
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-11-07 23:09:34 +0300

    Update grapth.cpp

--- pz4/grapth.cpp
+++ pz4/grapth.cpp
@@ -1,36 +1,87 @@
 #include <iostream>
-#include <list>
 
 using namespace std;
+
 class Graph
 {
+    bool** adjMatrix;
     int numVertices;
-    list<int> *adjLists;
 
 public:
-    Graph(int V);
-    void addEdge(int src, int dest);
+    Graph(int numVertices);
+    ~Graph();
+
+    void addEdge(int i, int j);
+    void removeEdge(int i, int j);
+    bool isEdge(int i, int j);
+    void table();
+
 };
 
-Graph::Graph(int vertices)
+Graph::Graph(int numVertices)
 {
-    numVertices = vertices;
-    adjLists = new list<int>[vertices];
+    this->numVertices = numVertices;
+    adjMatrix = new bool* [numVertices];
+
+    for (int i = 0; i < numVertices; i++)
+    {
+        adjMatrix[i] = new bool[numVertices];
+        for (int j = 0; j < numVertices; j++)
+            adjMatrix[i][j] = false;
+    }
 }
 
-void Graph::addEdge(int src, int dest)
+Graph::~Graph()
 {
-    adjLists[src].push_front(dest);
+    for (int i = 0; i < numVertices; i++)
+        delete[] adjMatrix[i];
+    delete[] adjMatrix;
 }
 
+void Graph::addEdge(int i, int j)
+{
+    adjMatrix[i][j] = true;
+    adjMatrix[j][i] = true;
+}
+
+void Graph::removeEdge(int i, int j)
+{
+    adjMatrix[i][j] = false;
+    adjMatrix[j][i] = false;
+}
+
+bool Graph::isEdge(int i, int j)
+{
+    return adjMatrix[i][j];
+}
+
+void Graph::table()
+{
+    for (int i = 0; i < numVertices; i++)
+    {
+        cout << i << " : ";
+        for (int j = 0; j < numVertices; j++)
+            cout << adjMatrix[i][j] << " ";
+        cout << endl;
+    }
+}
 
 int main()
 {
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
+    cout << "Enter number of vertics n endges" << endl;
+    int vert, edg;
+    cin >> vert >> edg;
 
-    return 0;
+    Graph g(vert);
+
+    cout << "Enter adjacent verics" << endl;
+
+    for (int i = 0; i < edg; i++)
+    {
+        int ver1, ver2;
+        cin >> ver1 >> ver2;
+        g.addEdge(ver1, ver2);
+    }
+
+    g.table();
 }
